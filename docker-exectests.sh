a3715934dd64a16a757a98a01adcace82e92d8a5
Bryon Bean <bryon.bean@kitware.com>
2017-08-19 09:55:37 -0400

    fix: fixes tests failing due to permissions of RSS dir bryon.bean@kitware.com

--- docker-exectests.sh
+++ docker-exectests.sh
@@ -4,6 +4,10 @@ RUNNER=`whoami`
 
 cd ~/cdash
 
+# ensures that out tests will not fail due to
+# permission problems on this directory
+sudo chmod 0777 public/rss/*.xml
+
 if [ -d "./$TEST_DIRECTORY" ]; then
     echo "Test directory, $TEST_DIRECTORY already exists."
     exit 1
@@ -11,5 +15,14 @@ fi
 
 echo "Running tests as $RUNNER."
 mkdir _build && cd _build
-cmake -DCDASH_TESTING_RENAME_LOGS=true -DCDASH_DB_HOST=mysql -DCDASH_DIR_NAME=cdash -DCDASH_DB_LOGIN=root  ..
+
+cmake \
+    -DCDASH_TESTING_RENAME_LOGS=true \
+    -DCDASH_DB_HOST=mysql \
+    -DCDASH_DIR_NAME=cdash \
+    -DCDASH_DB_LOGIN=root  \
+    -DCDASH_CONFIGURE_HTACCESS_FILE=ON \
+    ..
+
+chmod 0777 xdebugCoverage
 ctest --extra-verbose
