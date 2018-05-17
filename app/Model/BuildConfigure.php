bdf67c0fc096aa5ff99068dae56e1c0a1eb84bdf
Bryon Bean <bryon.bean@kitware.com>
2018-05-17 17:44:09 -0400

    refactor: align with latest arch changes

--- app/Model/BuildConfigure.php
+++ app/Model/BuildConfigure.php
@@ -18,8 +18,6 @@ namespace CDash\Model;
 use PDO;
 use CDash\Database;
 
-use CDash\Database;
-
 /** BuildConfigure class */
 class BuildConfigure
 {
@@ -377,7 +375,7 @@ class BuildConfigure
         return $response;
     }
 
-    public function getURL(int $default_id = null)
+    public function getURL($default_id = null)
     {
         $config = \CDash\Config::getInstance();
         $id = is_null($default_id) ? $this->BuildId : $default_id;
