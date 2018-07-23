9cbf13b21c2b5f7b3f4b001371ba098745c89ff1
Bryon Bean <bryon.bean@kitware.com>
2018-07-23 10:48:38 -0400

    fix: ensures request globals exist for call to CDash

--- app/Http/Controllers/CdashController.php
+++ app/Http/Controllers/CdashController.php
@@ -14,151 +14,167 @@ use Suin\RSSWriter\SimpleXMLElement;
 
 class CdashController extends Controller
 {
-    public function index(Request $request)
-    {
-      $path = $request->path();
-      $path = $path === '/' ? "/index.php" : $path;
+  public function cdash(Request $request)
+  {
+    $this->setRequestGlobals($request);
+    $path = $request->path();
+    $path = $path === '/' ? "/index.php" : $path;
 
-      /** @var FilesystemAdapter $cdashfs */
-      $cdashfs = Storage::disk('cdash');
+    /** @var FilesystemAdapter $cdashfs */
+    $cdashfs = Storage::disk('cdash');
 
-      if ($cdashfs->exists($path)) {
-        $file = $cdashfs->getDriver()
-            ->getAdapter()
-            ->applyPathPrefix($path);
-        chdir(app_path('CDash/public'));
+    if ($cdashfs->exists($path)) {
+      $file = $cdashfs->getDriver()
+        ->getAdapter()
+        ->applyPathPrefix($path);
+      chdir(app_path('CDash/public'));
 
+      ob_start();
+      $nocontent = require $file;
+      $content = ob_get_contents();
+      ob_end_clean();
 
-        ob_start();
-        $nocontent = require $file;
-        $content = ob_get_contents();
-        ob_end_clean();
+      $extension = pathinfo($path, PATHINFO_EXTENSION);
+      $type = "text/html";
 
-        $extension = pathinfo($path, PATHINFO_EXTENSION);
-        $type = "text/html";
-
-        switch ($extension)
-        {
-          case 'jpg':
-          case 'png':
-          case 'gif':
-          case 'jpeg':
-            $type = "image/{$extension}";
-            break;
-          case 'css':
-            $type = "text/css";
-            break;
-          case 'js':
-            $type = "application/javascript";
-            break;
-          default:
-            if (json_decode($content)) {
-              $type = "application/json";
-            }
+      switch ($extension) {
+        case 'jpg':
+        case 'png':
+        case 'gif':
+        case 'jpeg':
+          $type = "image/{$extension}";
+          break;
+        case 'css':
+          $type = "text/css";
+          break;
+        case 'js':
+          $type = "application/javascript";
+          break;
+        default:
+          if (json_decode($content)) {
+            $type = "application/json";
           }
-
-          if (is_array($nocontent) && isset($nocontent['Location'])) {
-            return redirect($nocontent['Location']);
-          }
-
-          $response = new Response($content);
-          return $response->header('Content-Type', $type);
-
-      } else {
-        $response = new Response('<strong>Not found</strong>', 404);
-        return $response->header('Content-Type', 'text/html');
       }
+
+      if (is_array($nocontent) && isset($nocontent['Location'])) {
+        return redirect($nocontent['Location']);
+      }
+
+      $response = new Response($content);
+      return $response->header('Content-Type', $type);
+
+    } else {
+      $response = new Response('<strong>Not found</strong>', 404);
+      return $response->header('Content-Type', 'text/html');
     }
+  }
 
-    public function submit(Request $request, Response $response)
-    {
-      // initialize action's variables
-      $md5 = null;
-      $message = '';
-      $status = 'ERROR';
-      $type = "application/{$this->expectsType()}";
-      $project_name = $request->query('project');
-      $email = Config::get('cdash.admin.email');
+  public function submit(Request $request, Response $response)
+  {
+    // initialize action's variables
+    $md5 = null;
+    $message = '';
+    $status = 'ERROR';
+    $type = "application/{$this->expectsType()}";
+    $project_name = $request->query('project');
+    $email = Config::get('cdash.admin.email');
 
-      $response->header('Content-Type', $type);
+    $response->header('Content-Type', $type);
 
-      try {
-        /** @var Project $project */
-        $project = Project::where('name', $project_name)->first();
+    try {
+      /** @var Project $project */
+      $project = Project::where('name', $project_name)->first();
 
-        // Project not found
-        if (!$project) {
-          $message = 'Not a valid project';
-          $content = $this->createView($status, $message);
-          return $response->setStatusCode(404)
-            ->setContent($content);
-        }
+      // Project not found
+      if (!$project) {
+        $message = 'Not a valid project';
+        $content = $this->createView($status, $message);
+        return $response->setStatusCode(404)
+          ->setContent($content);
+      }
 
-        // Project at build capacity
-        if ($project->isAtBuildCapacity()) {
-          $message = "Maximum number of builds reached for {$project->name}. Contact {$email} for support.";
-          $content = $this->createView($status, $message);
-          return $response->setStatusCode(409)->setContent($content);
-        }
+      // Project at build capacity
+      if ($project->isAtBuildCapacity()) {
+        $message = "Maximum number of builds reached for {$project->name}. Contact {$email} for support.";
+        $content = $this->createView($status, $message);
+        return $response->setStatusCode(409)->setContent($content);
+      }
 
-        $submission = $request->getContent(true);
+      $submission = $request->getContent(true);
 
-        /*
-         * This should look something like:
-         *   $factory = $this->>getXmlHandlerFactory();
-         *   $handler = $factory->create();
-         *   if ($handler->parse($submission)) {
-         *     $message = '';
-         *     ...
-         *    }
-         *
-         * Everything else in between should either be handled in middleware, in
-         * a different action (controller), or, possibly, not at all.
-         */
+      /*
+       * This should look something like:
+       *   $factory = $this->>getXmlHandlerFactory();
+       *   $handler = $factory->create();
+       *   if ($handler->parse($submission)) {
+       *     $message = '';
+       *     ...
+       *    }
+       *
+       * Everything else in between should either be handled in middleware, in
+       * a different action (controller), or, possibly, not at all.
+       */
 
-        // TODO: refactor out inclusion of this old code ASAP
-        // prevent old code from squawking
-        ob_start();
-        require_once app_path('CDash/include/do_submit.php');
-        do_submit($submission, $project->id);
-        // yikes
-        $output = ob_get_contents();
-        ob_end_clean();
+      // TODO: refactor out inclusion of this old code ASAP
+      // prevent old code from squawking
+      ob_start();
+      require_once app_path('CDash/include/do_submit.php');
+      do_submit($submission, $project->id);
+      // yikes
+      $output = ob_get_contents();
+      ob_end_clean();
 
-        $xml = new SimpleXMLElement($output);
-        if ($xml) {
-          $message = isset($xml->message) ? $xml->message : '';
-          $status = isset($xml->status) ? $xml->status : $status;
-          $md5 = isset($xml->md5) ? $xml->md5 : null;
-        } else {
-          $message = 'Unexpected output';
-          $response->setStatusCode(500);
-        }
-      } catch (\Exception $exception) {
-        $status = 'ERROR';
-        $message = $exception->getMessage();
+      $xml = new SimpleXMLElement($output);
+      if ($xml) {
+        $message = isset($xml->message) ? $xml->message : '';
+        $status = isset($xml->status) ? $xml->status : $status;
+        $md5 = isset($xml->md5) ? $xml->md5 : null;
+      } else {
+        $message = 'Unexpected output';
         $response->setStatusCode(500);
       }
-
-      $content = $this->createView($status, $message, $md5);
-
-      return $response->setContent($content);
+    } catch (\Exception $exception) {
+      $status = 'ERROR';
+      $message = $exception->getMessage();
+      $response->setStatusCode(500);
     }
 
-    protected function createView($status, $message, $md5 = null, $version = null)
-    {
-      $version = $version ?: Config::get('cdash.version.string');
-      $type = $this->expectsType();
+    $content = $this->createView($status, $message, $md5);
 
-      return View::make("cdash.{$type}")
-        ->with('version', $version)
-        ->with('status', $status)
-        ->with('message', $message)
-        ->with('md5', $md5);
+    return $response->setContent($content);
+  }
+
+  protected function createView($status, $message, $md5 = null, $version = null)
+  {
+    $version = $version ?: Config::get('cdash.version.string');
+    $type = $this->expectsType();
+
+    return View::make("cdash.{$type}")
+      ->with('version', $version)
+      ->with('status', $status)
+      ->with('message', $message)
+      ->with('md5', $md5);
+  }
+
+  protected function expectsType()
+  {
+    return Config::get('cdash.submissions.content_type');
+  }
+
+  protected function setRequestGlobals(Request $request)
+  {
+    foreach ($request->request as $name => $value) {
+      $GLOBALS['_POST'][$name] = $value;
+      $GLOBALS['_REQUEST'][$name] = $value;
     }
 
-    protected function expectsType()
-    {
-      return Config::get('cdash.submissions.content_type');
+    foreach ($request->query as $name => $value) {
+      $GLOBALS['_GET'][$name] = $value;
+      $GLOBALS['_REQUEST'][$name] = $value;
     }
+
+    foreach ($request->server as $name => $value) {
+      $_SERVER[$name] = $value;
+    }
+  }
 }
