995c0374e5365db9ed5b417b944adccdff89000b
Bryon Bean <bryon.bean@kitware.com>
2018-07-28 14:20:08 -0400

    refactor: DRYing and docs

--- tests/traits/CTestXMLSubmissions.php
+++ tests/traits/CTestXMLSubmissions.php
@@ -1,54 +1,116 @@
 <?php
+/**
+ * =========================================================================
+ *   Program:   CDash - Cross-Platform Dashboard System
+ *   Module:    $Id$
+ *   Language:  PHP
+ *   Date:      $Date$
+ *   Version:   $Revision$
+ *
+ *   Copyright (c) Kitware, Inc. All rights reserved.
+ *   See LICENSE or http://www.cdash.org/licensing/ for details.
+ *
+ *   This software is distributed WITHOUT ANY WARRANTY; without even
+ *   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *   PURPOSE. See the above copyright notices for more information.
+ * =========================================================================
+ */
+
 namespace CDash\Test\Traits;
 
-use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\File;
 
 trait CTestXMLSubmissions
 {
+  /** @var array $stubs */
   protected $stubs = [];
-  protected $projectName;
-  protected $dataDir;
 
+  /** @var string $projectName */
+  protected $projectName;
+
+  /**
+   * Submits a ctest xml file located in a previously intialized directory to an endpoint for
+   * processing
+   *
+   * @param $key
+   * @param string $method
+   * @param string $endpoint
+   * @return $this
+   */
   protected function assertCTestSubmissionOK($key, $method = 'PUT', $endpoint = '/submit')
   {
-    $file = "{$key}.xml";
-
-    if (isset($this->stubs[$file])) {
-      $content = $this->getRawFile($file);
-      $uri = "/{$endpoint}?project={$this->projectName}";
-      $server = [
-        'HTTP_CONTENT_LENGTH' => mb_strlen($content, '8bit'),
-        'CONTENT_TYPE' => 'application/xml',
-        'HTTP_ACCEPT' => 'application/xml',
-      ];
-
-      $this->call($method, $uri, [], [], [], $server, $content);
-      $actual = $this->response->getStatusCode();
-      $this->assertTrue(
-        $this->response->isOk(),
-        "Expected status code 200, got {$actual} trying to submit {$key}."
-      );
-    } else {
-      $this->assertTrue(false, "File, {$file}, not prepared for submission");
-    }
-
+    $content = $this->prepareSubmissionFile($key);
+    $uri = "/{$endpoint}?project={$this->projectName}";
+    $server = [
+      'HTTP_CONTENT_LENGTH' => mb_strlen($content, '8bit'),
+      'CONTENT_TYPE' => 'application/xml',
+      'HTTP_ACCEPT' => 'application/xml',
+    ];
+    $this->call($method, $uri, [], [], [], $server, $content);
+    $actual = $this->response->getStatusCode();
+    $this->assertTrue(
+      $this->response->isOk(),
+      "Expected status code 200, got {$actual} trying to submit {$key}."
+    );
+    $this->validateXmlStatus();
     return $this;
   }
 
-  protected function getFileForUpload($key)
+  /**
+   * Prepares a file that has been initialized for submission to endpoint.
+   *
+   * @param $file
+   * @return bool|string
+   */
+  protected function prepareSubmissionFile($file)
   {
+    $key = "{$file}.xml";
     if (isset($this->stubs[$key])) {
-      $path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . $key;
-
-      copy($this->stubs[$key], $path);
-      $size = filesize($path);
-      $type = 'text/xml';
-
-      return new UploadedFile($path, $key, $type, $size, null, true);
+      $content = $this->getRawFile($key);
+      return $content;
+    } else {
+      $this->fail("File, `$key', was not initialized.");
     }
   }
 
+  /**
+   * A successful submission will have a status equal to 'OK'
+   */
+  protected function validateXmlStatus()
+  {
+    $dom = $this->getResponseDOM();
+
+    /** @var \DOMElement $node */
+    foreach ($dom->childNodes as $node) {
+      if ($node->getNodePath() === '/cdash/status') {
+        $msg = "Expected OK, received {$node->nodeValue} from submission endpoint";
+        $this->assertEquals('OK', $node->nodeValue, $msg);
+      }
+    }
+  }
+
+  /**
+   * Converts the response xml from the submission endpoint into a DOMDocument
+   *
+   * @return \DOMDocument
+   */
+  protected function getResponseDOM()
+  {
+    $dom = new \DOMDocument();
+    try {
+      $dom->loadXML($this->response->getContent());
+    } catch (\Exception $e) {
+      $this->fail("Unable to parse response XML: {$e->getMessage()}");
+    }
+    return $dom;
+  }
+
+  /**
+   * Returns the contents of file in a directory that has been previously initialized
+   *
+   * @param $key
+   * @return bool|string
+   */
   protected function getRawFile($key)
   {
     if (isset($this->stubs[$key])) {
@@ -56,12 +118,13 @@ trait CTestXMLSubmissions
     }
   }
 
-  protected function getMd5HashForFile($key)
-  {
-    if (isset($this->stubs[$key])) {
-      return md5_file($this->stubs[$key]);
-    }
-  }
+  /**
+   * Initializes a project directory with all of the ctest xml files
+   *
+   * @param $directory
+   * @param null $project
+   * @param string $path
+   */
   protected function initProjectDirectory($directory, $project = null, $path = 'CDash.tests.data')
   {
     $this->projectName = $project ?: $directory;
